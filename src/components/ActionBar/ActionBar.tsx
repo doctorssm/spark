859f1ef7c8d5bcdcc16247886e6623903f672116
Sergii Bogush <sergii_bogush@epam.com>
2019-10-08 13:35:16 +0300

    ref

--- src/components/ActionBar/ActionBar.tsx
+++ src/components/ActionBar/ActionBar.tsx
@@ -5,13 +5,13 @@ export class ActionBar extends React.Component {
   render() {
     return (
       <div className="flex">
-        <Action icon={'thumbtack'} title={'Отметить как важное'} />
+        {/* <Action icon={'thumbtack'} title={'Отметить как важное'} /> */}
         <Action icon={'circle'} title={'Отметить как прочитанное'} />
-        <Action icon={'clock'} title={'Отложить'} />
-        <Action icon={'archive'} title={'В архив'} />
+        {/* <Action icon={'clock'} title={'Отложить'} /> */}
+        {/* <Action icon={'archive'} title={'В архив'} /> */}
         <Action icon={'trash-alt'} title={'Удалить'} />
-        <Action icon={'folder'} title={'Переместить'} />
-        <Action icon={'ellipsis-h'} title={'Еще'} />
+        {/* <Action icon={'folder'} title={'Переместить'} /> */}
+        {/* <Action icon={'ellipsis-h'} title={'Еще'} /> */}
       </div>
     )
   }
