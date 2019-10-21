7b6173efeed52ca61990be08fd786d6c5604df9a
Sergii Bogush <sergii_bogush@epam.com>
2019-10-21 15:30:22 +0300

    refactor: add types folder

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -4,15 +4,6 @@ import thunk from 'redux-thunk';
 
 import { reducers } from './app.reducer';
 
-// TODO: move to custom.d.ts
-declare global {
-  interface Window {
-    require: any;
-    ipcRenderer: any;
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
-  }
-}
-
 export const configureStore = () => {
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const middlewares: Middleware[] = process.env.NODE_ENV === 'development' ? [thunk, freeze] : [thunk];
---
+++ src/types/window.d.ts
@@ -0,0 +1,7 @@
+interface Window {
+  require: any;
+  ipcRenderer: any;
+  __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
+}
+
+declare const window: Window;
